e577df9f91f941f996e6916be12751b4b1c6ca54
dany2501 <dany2501.dhn@gmail.com>
2018-10-27 19:01:29 -0500

    first commit

--- public/javascripts/boton.js
+++ public/javascripts/boton.js
@@ -1,35 +1,27 @@
 $(document).ready(function(){
+
+    var form=$('#modal');
     var boton=$('#but');
     var agregar=$('#guardar');
-    var contenedor=$('#contenedor');
     var template="";
+    var escuelas =[];
+    var i=0;
 
     boton.click(function(){
-        console.log('Diste click');
+        form.slideDown("slow",function(){
+            console.log('Debió de haber bajado');
+        });
+
     });
+
+    
     agregar.click(function(){
-        var dato=$('#input').val();
-        template=` 
-        <div class="about-you-modal-container" id="modal">
-                            <div class="about-you-modal">
-                                <h2 class="heading-tertiary">
-                                    Grado Academico
-                                </h2>
-                                <input class="about-you-modal-input" type="text" id="input" placeholder="Ingresar....">
-                                <div class="about-you-btn-container">
-                                    <a class="btn btn-blue">Cerrar</a>
-                                    <a class="btn btn-blue" id="guardar">Guardar</a>
-                                    <a class="btn btn-blue">Eliminar</a>
-                                </div>
-                            </div>
-                        </div>
-        <a class="test" id="but" href="#modal">
-        <div class="about-you-feature selected-feature" href="#modal">
-            <div class="about-you-feature-information">
-                <p class="about-you-add-feature-information-text">Agregar</p>
-            </div>
-        </div>
-    </a>
+
+        var contenedor=$('#contenedor');
+        var dato=($('#input').val());
+        escuelas[i]=dato;
+        i++;
+        template+=`
         <div class="about-you-feature selected-feature">             
         <div class="about-you-feature-information">
             <p class="about-you-add-feature-information-text">
@@ -38,24 +30,12 @@ $(document).ready(function(){
 
         </div>
     </div>`;
+    console.log(escuelas);  
+    form.slideUp("fast",function(){
+        console.log('Debió de haber subido');
+    })
     contenedor.html(template);
-    // window.location.replace("http://localhost:3000/portafolio");
     });
-   /* $.ajax({
-        url:"http://localhost:3000/portafolio",
-        type:'post',
-        dataType:'json',
-        success : function(json) 
-        {
-            console.log(json);
-        },
-        error : function(xhr, status) {
-            alert('Existió un problema');
-        }
-
-
-        
-    });*/
 
 
 });
